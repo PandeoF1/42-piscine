723621d3f1f021d525d188ad7ac139a9a84fe921
Pandeo_F1 <theo.nard18@gmail.com>
2021-07-22 10:01:40 +0200

    update

---
+++ c08/ex04/ft_stock_str.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 09:40:36 by tnard             #+#    #+#             */
+/*   Updated: 2021/07/22 09:42:07 by tnard            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}				t_stock_str;
+#endif
