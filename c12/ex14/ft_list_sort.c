800a0ab2a749a0d63b8846e03026c4cc603c5580
tnard <theo.nard18@gmail.com>
2021-07-28 17:38:03 +0200

    Update ft_list_sort.c

--- c12/ex14/ft_list_sort.c
+++ c12/ex14/ft_list_sort.c
@@ -6,43 +6,13 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 13:47:52 by tnard             #+#    #+#             */
-/*   Updated: 2021/07/28 17:01:51 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2021/07/28 17:18:07 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_swap_list(t_list	*a, t_list	*b)
-{
-	void	*tmp;
-
-	tmp = a->data;
-	b->data = b->data;
-	b->data = tmp;
-}
-
-void	ft_list_sort(t_list **begin_list, int (*cmp)())
-{
-	t_list	*save;
-	t_list	*back;
-
-	save = *begin_list;
-	back = *begin_list;
-	while (*begin_list)
-	{	
-		(*begin_list) = (*begin_list)->next;
-		if (cmp(back->data, (*begin_list)->data) > 0)
-		{
-			ft_swap_list(&back, &begin_list);
-			begin_list = save;
-			begin_list = back;
-		}
-		else
-			if (*back)
-				(*back) = (*back)->next;
-	}
-}
-/*void	ft_swap_list(t_list	**a, t_list	**b)
+void	ft_swap_list(t_list	**a, t_list	**b)
 {
 	void	*tmp;
 
@@ -54,26 +24,26 @@ void	ft_list_sort(t_list **begin_list, int (*cmp)())
 void	ft_list_sort(t_list **begin_list, int (*cmp)())
 {
 	t_list	*tmp;
-	t_list	*curr;
-	int		i;
+	t_list	*current;
+	int		reset;
 
-	i = 1;
+	reset = 1;
 	if (!(*begin_list) || !(*begin_list)->next)
 		return ;
-	while (i)
+	while (reset)
 	{
-		i = 0;
+		reset = 0;
 		tmp = *begin_list;
-		curr = tmp->next;
-		while (curr)
+		current = tmp->next;
+		while (current)
 		{
-			if (cmp(tmp->data, curr->data) > 0)
+			if (cmp(tmp->data, current->data) > 0)
 			{
-				ft_swap_list(&tmp, &curr);
-				i = 1;
+				ft_swap_list(&tmp, &current);
+				reset = 1;
 			}
 			tmp = tmp->next;
-			curr = curr->next;
+			current = current->next;
 		}
 	}
-}*/
+}
