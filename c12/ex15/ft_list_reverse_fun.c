55d2df8c8997a05012e5312da64a8a2b8b09cfc0
tnard <theo.nard18@gmail.com>
2021-07-29 16:01:33 +0200

    Update ft_list_reverse_fun.c

--- c12/ex15/ft_list_reverse_fun.c
+++ c12/ex15/ft_list_reverse_fun.c
@@ -6,24 +6,57 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 12:54:31 by tnard             #+#    #+#             */
-/*   Updated: 2021/07/23 13:42:03 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2021/07/29 15:16:21 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
-void	ft_list_reverse_fun(t_list **begin_list)
+int	ft_list_size(t_list *begin_list)
 {
-	t_list	*a;
-	t_list	*b;
+	int		i;
+	t_list	*elem;
 
-	a = 0;
-	while (*begin_list)
+	elem = begin_list;
+	i = 0;
+	while (elem != NULL)
 	{
-		b = (*begin_list)->next;
-		(*begin_list)->next = a;
-		a = *begin_list;
-		*begin_list = b;
+		elem = elem->next;
+		i++;
 	}
-	*begin_list = a;
+	return (i);
+}
+
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	t_list			*tmp;
+	unsigned int	i;
+
+	i = -1;
+	tmp = begin_list;
+	while (++i < nbr)
+	{
+		if (!tmp)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
+
+void	ft_list_reverse_fun(t_list *begin_list)
+{
+	int		i;
+	int		len;
+	void	*temp;
+
+	i = -1;
+	len = ft_list_size(begin_list);
+	while (++i < len / 2)
+	{
+		temp = ft_list_at(begin_list, i)->data;
+		ft_list_at(begin_list, i)->data
+		= ft_list_at(begin_list, len - i - 1)->data;
+		ft_list_at(begin_list, len - i - 1)->data = temp;
+	}
 }
